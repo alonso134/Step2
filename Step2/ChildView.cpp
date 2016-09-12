c4c8115530c8c1c017a6bcf786db71fe5988fe6a
alonso134 <xiaokaic@msu.edu>
2016-09-12 02:24:11 -0400

    Step 2: Brushes and Rectangles (Finished) ->Step 2: Fonts and Text

--- Step2/ChildView.cpp
+++ Step2/ChildView.cpp
@@ -1,3 +1,9 @@
+/**
+ * \file ChildView.cpp
+ *
+ * \author Kaichen Xiao
+ */
+
 
 // ChildView.cpp : implementation of the CChildView class
 //
@@ -10,13 +16,20 @@
 #define new DEBUG_NEW
 #endif
 
+using namespace Gdiplus;
 
 // CChildView
 
+/**
+* Constructor
+*/
 CChildView::CChildView()
 {
 }
 
+/**
+* Destructor
+*/
 CChildView::~CChildView()
 {
 }
@@ -30,6 +43,10 @@ END_MESSAGE_MAP()
 
 // CChildView message handlers
 
+/** This function is called before the window is created.
+* \param cs A structure with the window creation parameters
+* \returns TRUE
+*/
 BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
 {
 	if (!CWnd::PreCreateWindow(cs))
@@ -43,10 +60,21 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 	return TRUE;
 }
 
+/** This function is called to draw in the window.
+*
+* This function is called in response to a drawing message
+* whenever we need to redraw the window on the screen.
+* It is responsible for painting the window.
+*/
 void CChildView::OnPaint() 
 {
 	CPaintDC dc(this); // device context for painting
+	Graphics graphics(dc.m_hDC); // Create GDI+ graphics context
 	
+	Pen pen(Color(0, 128, 0), 3);
+	SolidBrush brush(Color(0, 0, 255));
+	graphics.FillRectangle(&brush, 100, 125, 400, 50);
+
 	// TODO: Add your message handler code here
 	
 	// Do not call CWnd::OnPaint() for painting messages
--- Step2/ChildView.h
+++ Step2/ChildView.h
@@ -1,3 +1,14 @@
+/**
+ * \file ChildView.h
+ *
+ * \author Kaichen Xiao
+ * 
+ * Class that implements the child window our program draws in.
+ *
+ * The window is a child of the main frame, which holds this 
+ * window, the menu bar, and the status bar.
+ */
+
 
 // ChildView.h : interface of the CChildView class
 //
@@ -8,6 +19,9 @@
 
 // CChildView window
 
+/**
+* The child window our program draws in.
+*/
 class CChildView : public CWnd
 {
 // Construction
